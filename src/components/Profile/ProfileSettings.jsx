74ad8e7aeeed9fbabd0d575418d6b991bdbf602e
Felix <dexdaprotocol@gmail.com>
2022-06-08 17:19:42 +0200

    Update ProfileSettings.jsx

--- src/components/Profile/ProfileSettings.jsx
+++ src/components/Profile/ProfileSettings.jsx
@@ -19,9 +19,9 @@ export default function ProfileSettings() {
     //profile
     const userName = document.getElementById('userName').value
     const userBio = document.getElementById('userBio').value
-    const artistName = document.getElementById('artistName').value
+    // const artistName = document.getElementById('artistName').value
     const userUrl = document.getElementById('userUrl').value
-    const userEmail = document.getElementById('email').value
+    // const userEmail = document.getElementById('email').value
     const userImg = document.getElementById('userImg').files[0]
     const coverImg = document.getElementById('coverImg').files[0]
 
@@ -227,7 +227,7 @@ export default function ProfileSettings() {
       </div>
 
       <div className="mt-6 grid grid-cols-12 gap-6">
-        <div className="col-span-12 sm:col-span-6">
+        {/* <div className="col-span-12 sm:col-span-6">
           <label
             htmlFor="first-name"
             className="block text-sm font-medium text-gray-700"
@@ -243,7 +243,7 @@ export default function ProfileSettings() {
             autoComplete="given-name"
             className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-teal-500 focus:outline-none focus:ring-teal-500 sm:text-sm"
           />
-        </div>
+        </div> */}
 
         <div className="col-span-12">
           <label
